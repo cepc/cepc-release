5df993101d6f567a4d1363c265e8e5b3258a33a3
Xianghu Zhao <zhaoxh@ihep.ac.cn>
2017-09-08 10:05:46 +0800

    Fix tar bug

--- handler/tar.py
+++ handler/tar.py
@@ -2,7 +2,7 @@ import os
 from cepcenv.util import call
 
 def extract(param):
-    tar_file = os.path.join(param['pkg_config']['download_dir'], param['action_param']['file'])
+    tar_file = os.path.join(param['pkg_config']['download_dir'], param['action_param']['file'].format(**param['pkg_config']))
     dst_dir = param['pkg_config']['source_dir']
     cmd = ['tar', 'xvzf', tar_file]
     ret, out, err = call(cmd, cwd=dst_dir)
