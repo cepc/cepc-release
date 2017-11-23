295f1bc71157f37477f30f090b49e96ee4614c1a
Xianghu Zhao <zhaoxh@ihep.ac.cn>
2017-11-23 09:38:20 +0800

    Run through the installation

--- handler/util.py
+++ handler/util.py
@@ -3,7 +3,7 @@ import multiprocessing
 def auto_make_jobs(make_opt):
     for opt in make_opt:
         if opt.startswith('-j'):
-            break
+            return make_opt
 
     make_opt.insert(0, '-j{0}'.format(multiprocessing.cpu_count()))
     return make_opt
